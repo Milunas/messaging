8e0df05379c62c426c7789cac0cc94b6a7401772
Łukasz Milunas <milunas.lukasz@gmail.com>
2019-02-28 16:11:43 +0100

    stream properties

--- message-listener/src/main/java/com/message/milunas/messagelistener/MessageListenerApplication.java
+++ message-listener/src/main/java/com/message/milunas/messagelistener/MessageListenerApplication.java
@@ -2,8 +2,11 @@ package com.message.milunas.messagelistener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class MessageListenerApplication {
 
 	public static void main(String[] args) {
--- message-publisher/src/main/java/com/message/milunas/messagepublisher/MessagePublisherApplication.java
+++ message-publisher/src/main/java/com/message/milunas/messagepublisher/MessagePublisherApplication.java
@@ -2,8 +2,11 @@ package com.message.milunas.messagepublisher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
+@EnableBinding(Source.class)
 public class MessagePublisherApplication {
 
 	public static void main(String[] args) {
