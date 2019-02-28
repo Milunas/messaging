9b4c1d08a07cc8e835ad955b940382ea7618b3e6
Łukasz Milunas <milunas.lukasz@gmail.com>
2019-02-28 16:33:20 +0100

    listener

---
+++ message-listener/src/main/java/com/message/milunas/messagelistener/listener/Listener.java
@@ -0,0 +1,21 @@
+package com.message.milunas.messagelistener.listener;
+
+import com.message.milunas.dto.event.Event;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Listener {
+
+    private static final Logger log = LoggerFactory.getLogger(Listener.class);
+
+    @StreamListener(target = Sink.INPUT)
+    public void receiveGranted(@Payload Event event) {
+        log.info("Event occurred: {}", event);
+    }
+
+}
